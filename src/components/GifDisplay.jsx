6aeb7d8447d50ccb74f76733ba9e78013df3d354
NoeParedes <ananias.paredes@utec.edu.pe>
2024-07-16 23:59:09 -0500

    Updated GIF

--- src/components/GifDisplay.jsx
+++ src/components/GifDisplay.jsx
@@ -5,7 +5,10 @@ function GifDisplay() {
   return (
     <section className="my-12">
       <div className="max-w-2xl mx-auto">
-        <img src={HuellaAzulGIF} alt="La Huella Azul GIF" className="w-15 md:w-full rounded-lg shadow-xl" />
+        <figure>
+          <img src={HuellaAzulGIF} alt="La Huella Azul GIF" className="w-15 md:w-full rounded-lg shadow-xl" />
+          <figcaption className="text-center mt-2 text-sm text-gray-600">La Huella Azul</figcaption>
+        </figure>
       </div>
     </section>
   );
