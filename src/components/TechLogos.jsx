f73e428190886040b240142c1436f6aabdcf2239
OmarUTEC <javier.chavarria@utec.edu.pe>
2024-07-17 00:53:43 -0500

    create icons

--- src/components/TechLogos.jsx
+++ src/components/TechLogos.jsx
@@ -7,7 +7,7 @@ import gorgeo from '../assets/gorjeo.png';
 import wifi from '../assets/logotipo-de-wifi.png';
 import cromo from '../assets/cromo.png';
 import apple from '../assets/logotipo-de-apple (1).png';
-import bluetouch from '../assets/bluetooth (1).png';
+import bluetooth from '../assets/bluetooth (1).png';
 import discord from '../assets/discordia (1).png';
 import correo from '../assets/correo (1).png';
 import linkedin from '../assets/linkedin.png';
@@ -19,35 +19,33 @@ import reddit from '../assets/reddit.png';
 import windows from '../assets/silueta-del-logotipo-de-windows.png';
 import idm from '../assets/ibm.png';
 import huawei from '../assets/huawei.png';
-import andriod from '../assets/simbolo-de-caracter-de-android.png';
+import android from '../assets/simbolo-de-caracter-de-android.png';
 import tesla from '../assets/tesla.png';
 import intel from '../assets/intel.png';
 
 const logos = [
-  { src: nvidiaLogo, name: 'NVIDIA', year: 1993 },
-  { src: fb, name: 'FACEBOOK', year: 1993 },
-  { src: chatgpt, name: 'CHATGPT', year: 2020 },
-  { src: whatsapp, name: 'WHATSAPP', year: 2009 },
-  { src: gorgeo, name: 'GORGEO', year: 2021 },
-  { src: wifi, name: 'WIFI', year: 1997 },
-  { src: cromo, name: 'CROMO', year: 2008 },
-  { src: apple, name: 'APPLE', year: 1976 },
-  { src: bluetouch, name: 'BLUETOOTH', year: 1994 },
-  { src: discord, name: 'DISCORD', year: 2015 },
-  { src: correo, name: 'CORREO', year: 1971 },
-  { src: linkedin, name: 'LINKEDIN', year: 2002 },
-  { src: youtube, name: 'YOUTUBE', year: 2005 },
-  { src: telegram, name: 'TELEGRAM', year: 2013 },
-  { src: twitch, name: 'TWITCH', year: 2011 },
-  { src: spotify, name: 'SPOTIFY', year: 2006 },
-  { src: reddit, name: 'REDDIT', year: 2005 },
-  { src: windows, name: 'WINDOWS', year: 1985 },
-  { src: idm, name: 'IDM', year: 1999 },
-  { src: huawei, name: 'HUAWEI', year: 1987 },
-  { src: andriod, name: 'ANDRIOD', year: 2003 },
-  { src: tesla, name: 'TESLA', year: 2003 },
   { src: intel, name: 'INTEL', year: 1968 },
-
+  { src: correo, name: 'CORREO', year: 1971 },
+  { src: apple, name: 'APPLE', year: 1976 },
+  { src: windows, name: 'WINDOWS', year: 1985 },
+  { src: huawei, name: 'HUAWEI', year: 1987 },
+  { src: nvidiaLogo, name: 'NVIDIA', year: 1993 },
+  { src: bluetooth, name: 'BLUETOOTH', year: 1994 },
+  { src: wifi, name: 'WIFI', year: 1997 },
+  { src: idm, name: 'IDM', year: 1999 },
+  { src: linkedin, name: 'LINKEDIN', year: 2002 },
+  { src: android, name: 'ANDROID', year: 2003 },
+  { src: tesla, name: 'TESLA', year: 2003 },
+  { src: youtube, name: 'YOUTUBE', year: 2005 },
+  { src: reddit, name: 'REDDIT', year: 2005 },
+  { src: spotify, name: 'SPOTIFY', year: 2006 },
+  { src: gorgeo, name: 'GORGEO', year: 2006 },
+  { src: cromo, name: 'CROMO', year: 2008 },
+  { src: whatsapp, name: 'WHATSAPP', year: 2009 },
+  { src: twitch, name: 'TWITCH', year: 2011 },
+  { src: telegram, name: 'TELEGRAM', year: 2013 },
+  { src: discord, name: 'DISCORD', year: 2015 },
+  { src: chatgpt, name: 'CHATGPT', year: 2022 }, // Actualización de la fecha de ChatGPT
 ];
 
 function TechLogos() {
